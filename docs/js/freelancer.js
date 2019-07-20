f29de141b34e76704962ed1426f6f274a500ce35
Buttenup <50253991+Buttenup@users.noreply.github.com>
2019-07-20 19:58:55 +1000

    Update freelancer.js

--- docs/js/freelancer.js
+++ docs/js/freelancer.js
@@ -10,7 +10,7 @@
           const imgNo = Math.round(100*Math.random()) % 2;
 
         //James change here:
-          let percent = 12;
+          let percent = 7;
            $("#energy-msg1").text(+ percent + "% renewables right now");
           if(percent < 10) {
                 $("#energy-msg2").text("Postpone heavy power usage if possible");
