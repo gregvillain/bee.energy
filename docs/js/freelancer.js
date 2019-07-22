f7970e5f702f4676954c09f17cca7c5f8ce47366
Radek Ostrowski <radek@startonchain.com>
2019-07-22 11:39:13 +1000

    update

--- docs/js/freelancer.js
+++ docs/js/freelancer.js
@@ -1,44 +1,113 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  fetch('https://api.bee.energy:8000/current')
-    .then(function(response) {
-      response.text().then(function (text) {
-          const json = JSON.parse(text);
-          let price = Math.round(json.spotPrice/10);
-          const d = new Date();
-          const imgNo = Math.round(100*Math.random()) % 2;
+fetch("https://data.opennem.org.au/power/nsw1.json")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+      const black_coal = x[0].history.data;
+      const distillate = x[1].history.data;
+      const gas_ccgt = x[2].history.data;
+      const gas_ocgt = x[3].history.data;
+      const hydro = x[4].history.data;
+      const pumps = x[5].history.data;
+      const solar = x[6].history.data;
+      const wind = x[7].history.data;
+      const exports = x[8].history.data;
+      const imports = x[9].history.data;
+      const rooftop_solar = x[10].forecast.data;
 
-        //James change here:
-          let percent = 8;
-           $("#energy-msg1").text(+ percent + "% renewables right now");
-          if(percent < 10) {
-                $("#energy-msg2").text("Postpone heavy power usage if possible");
-                $("#background").removeClass("bg-primary");
-                $("#background").addClass("bg-danger");
-                $("#background1").removeClass("bg-primary");
-                $("#background1").addClass("bg-danger");
-                $("#avatar").attr("src","img/scared-" + imgNo + ".svg");
-          } else {
-                let r = [
-                    "Roast that chicken!",
-                    "Grill the veggies!",
-                    "Do your laundry!",
-                    "Iron your shirts!",
-                    "Dry your clothes!",
-                    "Make a cuppa!",
-                    "Vacuum the carpet!"];
-                let rMsg = r[Math.round(100*Math.random()) % r.length];
-                $("#energy-msg2").text(rMsg);
-                $("#background").addClass("bg-primary");
-                $("#background").removeClass("bg-danger");
-                $("#background1").addClass("bg-primary");
-                $("#background1").removeClass("bg-danger");
-                $("#avatar").attr("src","img/happy-" + imgNo + ".svg");
-          }
-          $("#price").text("Current energy price is " + price + " cents per kWh.");
-      });
-    });
+      const black_coal_value = black_coal[black_coal.length-1];
+      const distillate_value = distillate[distillate.length-1];
+      const gas_ccgt_value = gas_ccgt[gas_ccgt.length-1];
+      const gas_ocgt_value = gas_ocgt[gas_ocgt.length-1];
+      const hydro_value = hydro[hydro.length-1];
+      const pumps_value = pumps[pumps.length-1];
+      const solar_value = solar[solar.length-1];
+      const wind_value = wind[wind.length-1];
+      const exports_value = exports[exports.length-1];
+      const imports_value = imports[imports.length-1];
+      const rooftop_solar_value = rooftop_solar[rooftop_solar.length-1];
+
+      const total = black_coal_value + distillate_value + gas_ccgt_value +
+                    gas_ocgt_value + hydro_value + pumps_value + solar_value +
+                    wind_value + rooftop_solar_value - imports_value;
+      const renewables = hydro_value + solar_value + wind_value + rooftop_solar_value;
+
+      const percent = Math.round(renewables/total*100);
+      const price = Math.round(x[11].history.data[x[11].history.data.length-1]/10);
+
+                const d = new Date();
+                const imgNo = Math.round(100*Math.random()) % 2;
+
+                 $("#energy-msg1").text(+ percent + "% renewables right now");
+                if(percent < 10) {
+                      $("#energy-msg2").text("Postpone heavy power usage if possible");
+                      $("#background").removeClass("bg-primary");
+                      $("#background").addClass("bg-danger");
+                      $("#background1").removeClass("bg-primary");
+                      $("#background1").addClass("bg-danger");
+                      $("#avatar").attr("src","img/scared-" + imgNo + ".svg");
+                } else {
+                      let r = [
+                          "Roast that chicken!",
+                          "Grill the veggies!",
+                          "Do your laundry!",
+                          "Iron your shirts!",
+                          "Dry your clothes!",
+                          "Make a cuppa!",
+                          "Vacuum the carpet!"];
+                      let rMsg = r[Math.round(100*Math.random()) % r.length];
+                      $("#energy-msg2").text(rMsg);
+                      $("#background").addClass("bg-primary");
+                      $("#background").removeClass("bg-danger");
+                      $("#background1").addClass("bg-primary");
+                      $("#background1").removeClass("bg-danger");
+                      $("#avatar").attr("src","img/happy-" + imgNo + ".svg");
+                }
+                $("#price").text("Current energy price is " + price + " cents per kWh.");
+
+  });
+
+//  fetch('https://api.bee.energy:8000/current')
+//    .then(function(response) {
+//      response.text().then(function (text) {
+//          const json = JSON.parse(text);
+//          let price = Math.round(json.spotPrice/10);
+//          const d = new Date();
+//          const imgNo = Math.round(100*Math.random()) % 2;
+//
+//        //James change here:
+//          let percent = 8;
+//           $("#energy-msg1").text(+ percent + "% renewables right now");
+//          if(percent < 10) {
+//                $("#energy-msg2").text("Postpone heavy power usage if possible");
+//                $("#background").removeClass("bg-primary");
+//                $("#background").addClass("bg-danger");
+//                $("#background1").removeClass("bg-primary");
+//                $("#background1").addClass("bg-danger");
+//                $("#avatar").attr("src","img/scared-" + imgNo + ".svg");
+//          } else {
+//                let r = [
+//                    "Roast that chicken!",
+//                    "Grill the veggies!",
+//                    "Do your laundry!",
+//                    "Iron your shirts!",
+//                    "Dry your clothes!",
+//                    "Make a cuppa!",
+//                    "Vacuum the carpet!"];
+//                let rMsg = r[Math.round(100*Math.random()) % r.length];
+//                $("#energy-msg2").text(rMsg);
+//                $("#background").addClass("bg-primary");
+//                $("#background").removeClass("bg-danger");
+//                $("#background1").addClass("bg-primary");
+//                $("#background1").removeClass("bg-danger");
+//                $("#avatar").attr("src","img/happy-" + imgNo + ".svg");
+//          }
+//          $("#price").text("Current energy price is " + price + " cents per kWh.");
+//      });
+//    });
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
