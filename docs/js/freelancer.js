346e2e5db30bcffde0e8464822f3e510423a1410
Buttenup <50253991+Buttenup@users.noreply.github.com>
2019-07-26 10:37:31 +1000

    Update freelancer.js

--- docs/js/freelancer.js
+++ docs/js/freelancer.js
@@ -37,7 +37,10 @@
           const renewables = hydro_value + solar_value + wind_value + rooftop_solar_value;
 
           const percent = Math.round(renewables/total*100);
-          const price = Math.round(x[11].history.data[x[11].history.data.length-1]/10);
+          // const price = Math.round(x[11].history.data[x[11].history.data.length-1]/10);
+         const avg_renewables = 15;
+         const avg_price = 0.07;
+         const price = avg_price - ((percent - avg_renewables)/3);
 
                     const d = new Date();
                     const imgNo = Math.round(100*Math.random()) % 2;
