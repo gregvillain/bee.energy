e39a975d77ff6de4f1dc0d1efd4cf04d6c8a43a7
Radek Ostrowski <radek@startonchain.com>
2019-07-29 17:27:54 +1000

    checking no-cors

--- docs/js/freelancer.js
+++ docs/js/freelancer.js
@@ -1,8 +1,26 @@
 (function($) {
   "use strict"; // Start of use strict
 
+function getData(url = '') {
+  // Default options are marked with *
+    return fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            'IFTTT-Channel-Key': 'bee.energy'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        //body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json()); // parses JSON response into native JavaScript objects
+}
+
   function update() {
-       fetch("https://data.opennem.org.au/power/nsw1.json")
+       getData("https://data.opennem.org.au/power/nsw1.json")
       .then(function(response) {
         return response.json();
       })
